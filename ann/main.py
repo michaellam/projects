6067eb72b242bbe3386ffa67c52291b58767f38d
michaellam <michaeljosephlam@gmail.com>
2015-08-02 17:06:19 -0700

    example of simple pattern training

--- ann/main.py
+++ ann/main.py
@@ -1,5 +1,15 @@
-import NeuralNetwork as ann
+import NeuralNetwork
 
-net = ann.NeuralNetwork(3, sizes=[2,2,1])
 
-print net.neural_network[0].layer[0].type
+# example of a network learning a simple pattern
+input_vector = [1,0,1,0]
+target_vector = [1,0]
+nn = NeuralNetwork.NeuralNetwork(4, learning_rate=1, sizes=[3, 2], minimum_error=0.10, verbose=False)
+
+errors = []
+for x in range (0, 100):
+    nn.forward_pass(input_vector, target_vector)
+    nn.back_propogate_error()
+    nn.back_propogate_weights()
+    errors.append(nn.get_total_output_error())
+print errors
